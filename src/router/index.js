383d2f308b53abe6d282ef1a075e93823daf7fa6
KillingJacky <jacky.shaoxg@gmail.com>
2020-03-08 14:09:45 +0800

    fix: fix the blank screen after packed

--- src/router/index.js
+++ src/router/index.js
@@ -21,7 +21,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash', //histroy mode not work for electron-builder production pack
   base: process.env.BASE_URL,
   routes
 })
