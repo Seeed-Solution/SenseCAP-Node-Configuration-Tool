4ea4b8b2d84bc87942967f5ad062b0644c3b1492
Jack Shao <jacky.shaoxg@gmail.com>
2022-06-10 18:26:39 +0800

    feat: update node version to 16, bump node-sass to v6.0.1, update serialport

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { notarize } = require('electron-notarize');
 
 exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;  
+  const { electronPlatformName, appOutDir } = context;
   if (electronPlatformName !== 'darwin') {
     return;
   }
@@ -14,5 +14,6 @@ exports.default = async function notarizing(context) {
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEIDPASS,
+    ascProvider: process.env.APPLEASCPROVIDER
   });
 };
